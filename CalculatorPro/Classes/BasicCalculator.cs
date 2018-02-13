e880ebfb705c0892c0075510f21433c561479cde
Przemek Wojtarowicz <infanymousss@gmail.com>
2018-02-13 02:02:25 +0100

    chyba dodalem folder klasy do repo

---
+++ CalculatorPro/Classes/BasicCalculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorPro.Classes
+{
+    public class BasicCalculator
+    {
+        string expression;
+
+        public string Expression { get { return expression; } set { expression = value; } }
+
+
+    }
+}
